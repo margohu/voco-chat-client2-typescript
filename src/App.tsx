d440c68729ed77668d51488fe2d8fadd7ce42a2a
Margo Hutt <margo.hutt@ir.ee>
2022-05-04 22:00:42 +0300

    Final

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,21 @@
 import React, { useEffect } from 'react';
 import './App.css';
-import Chats from "./components/chat/Chat";
+import Chat from './Components/Chat';
 import { disconnectSocket, connectToServer } from './socket-service';
 
 function App() {
     useEffect(() => {
-        connectToServer()
-        .then((message:any) => {
-            console.log(message);
-        });
+
+        connectToServer();
         return () => {
             disconnectSocket();
         }
     }, []);
 
     return (
-        <div class="chat-conteiner">
+        <>
             <Chat />
-        <div/>
-        <div className="btn-container">
-            <input placeholder="Autor"/>
-            <input placeholder="Sõnum"/>
-            <input placeholder="Saada"/>
-            <button className='buttonSend'>Saada</button>
-        </div>
-    </div>
+        </>
     );
 }
 
